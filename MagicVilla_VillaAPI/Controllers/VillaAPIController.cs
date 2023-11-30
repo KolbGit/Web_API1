5a56b6da96277aa75434021b94f474602f09f205
Eugen <zenyakolb@gmail.com>
2023-11-30 19:57:47 +0300

    Section 2 - VillaDTO

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,4 +1,5 @@
 ﻿using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers
@@ -8,11 +9,11 @@ namespace MagicVilla_VillaAPI.Controllers
     public class VillaAPIController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<Villa> GetVillas()
+        public IEnumerable<VillaDTO> GetVillas()
         {
-            return new List<Villa> {
-            new Villa{Id=1,Name="Pool View" },
-            new Villa{Id=2,Name="Beach View" }
+            return new List<VillaDTO> {
+            new VillaDTO{Id=1,Name="Pool View" },
+            new VillaDTO{Id=2,Name="Beach View" }
             };
         }
     }
